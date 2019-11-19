f70a05170f8200a89ce25366f13d7eab89a20fe4
SweetWeakness <shirshin.dg@phystech.edu>
2019-11-19 19:12:46 +0300

    b problem (2nd module) remake

--- mod1_a.cpp
+++ mod1_a.cpp
@@ -1,49 +1,64 @@
+/*
+ * Найдите все вхождения шаблона в строку. Длина шаблона – p, длина строки – n. Время O(n + p), доп. память – O(p).
+p <= 30000, n <= 300000.
+Использовать один из методов:
+- С помощью префикс-функции;
+- С помощью z-функции.
+ */
+
 #include <iostream>
 #include <vector>
 
 
-void prefix_function (std::string& s, std::vector<int>& pi) {
-    int n = s.length();
+// Compute Prefix Function
+// O(p) mem
+std::vector<int> CMP (std::string& p) {
+    int m = p.length();
+    std::vector<int> pi(m, 0);
+    pi[0] = 0;
+    int k = 0;
 
-    for (int i=1; i<n; ++i) {
-        int j = pi[i-1];
+    for (int q = 1; q < m; ++q) {
+        while (k > 0 && pi[k] != pi[q]) {
+            k = pi[k - 1];
+        }
+        if (p[k] == p[q]) {
+            ++k;
+        }
+        pi[q] = k;
+    }
 
-        while (j > 0 && s[i] != s[j])
-            j = pi[j-1];
+    return pi;
+}
 
-        if (s[i] == s[j])
-            ++j;
+// Knuth - Morris - Pratt algorithm
+void KMP_Matcher (std::string &t, std::string& p) {
+    int n = t.length();
+    int m = p.length();
+    std::vector<int> pi = CMP(p);
 
-        pi[i] = j;
+    int q = 0;
+
+    for (int i = 0; i < n; ++i) {
+        while (q > 0 && p[q] != t[i]) {
+            q = pi[q - 1];
+        }
+        if (p[q] == t[i]){
+            ++q;
+        }
+        if (q == m) {
+            std:: cout << i - m + 1 <<" ";
+            q = pi[q - 1];
+        }
     }
 }
 
 int main() {
-    std::string s, pattern;
-    std::cin >> pattern;
+    std::string s, p;
+    std::cin >> p;
     std::cin >> s;
-    s = pattern + '#' + s;
-    int n = s.length();
 
-    std::vector<int> pi (n, 0);
-    prefix_function(s, pi);
+    KMP_Matcher(s, p);
 
-    for (int i=2*pattern.length(); i<n; ++i) {
-        if (pi[i] == pattern.length()){
-            std::cout << i - 2*pattern.length() << " ";
-        }
-    }
-
-    /*
-    std::cout << "\n";
-    for (int i=0; i<n; ++i) {
-        std::cout << s[i] << " ";
-    }
-
-    std::cout << "\n";
-    for (int i=0; i<n; ++i) {
-        std::cout << pi[i] << " ";
-    }
-     */
     return 0;
 }
