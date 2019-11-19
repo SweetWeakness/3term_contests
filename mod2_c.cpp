45b37358fde7623ece968fe53228ce09e1eb9ec4
SweetWeakness <shirshin.dg@phystech.edu>
2019-11-20 01:07:31 +0300

    c remake (2nd module)

--- mod2_c.cpp
+++ mod2_c.cpp
@@ -4,28 +4,42 @@
  * Найдите k-ую в лексикографическом порядке строку из этого множества.
  * Полезная статья про сравнение указателей: https://stackoverflow.com/questions/9086372/how-to-compare-pointers.
 */
+
+
 #include <iostream>
 #include <vector>
 
 
-void sufix_array(std::string& s, int alphabet, std::vector<int>& suffs) {
-    int n = s.size();
-    int cnt_len = (n > alphabet ? n : alphabet);
-    std::vector<int> cnt(cnt_len, 0);
-    std::vector<int> classes(n, 0);
+// сортируем посимвольно строку
+std::vector<int> zero_iteration (const std::string &s, const int alphabet) {
+    const int n = s.length();
+    std::vector<int> suffs (n, 0); // for return
+    std::vector<int> cnt (alphabet, 0);
 
-    for (int i = 0; i < n; ++i) { // карманная сортировка подсчетом
+    // карманная сортировка подсчетом
+    for (int i = 0; i < n; ++i) {
         ++cnt[s[i] - '\0'];
     }
-    for (int i = 1; i < alphabet; ++i) // считаем границы
-        cnt[i] += cnt[i-1];
+    // считаем границы
+    for (int i = 1; i < alphabet; ++i) {
+        cnt[i] += cnt[i - 1];
+    }
     // suffs будет хранить индексы начал отсортированных подстрок текущей длины
     for (int i = 0; i < n; ++i) {
         suffs[--cnt[s[i] - '\0']] = i;
     }
 
+    return suffs;
+}
+
+
+// создаем классы для последующих итераций
+std::vector<int> make_classes (const std::string& s, const std::vector<int>& suffs) {
+    const int n = s.length();
+    std::vector<int> classes (n, 0); // for return
     int class_numb = 0;
     char last_char = '$';
+
     // каждому суффиксу длины 1 сопоставляем класс
     for (int i = 0; i < n; ++i) {
         if (s[suffs[i]] != last_char){
@@ -35,22 +49,27 @@ void sufix_array(std::string& s, int alphabet, std::vector<int>& suffs) {
         classes[suffs[i]] = class_numb;
     }
 
+    return classes;
+}
 
 
+// сортируем циклические сдвиги строки длины степеней двойки (2, 4, 8, ... n)
+void main_iterations (std::vector<int>& suffs, std::vector<int>& classes) {
+    const int n = suffs.size();
+    std::vector<int> cnt (n, 0);
+    std::vector<int> sorted_by2 (n, 0);
+    std::vector<int> new_classes (n, 0);
 
-    // нулевая итерация завершена
-    // сортируем подстроки длиной 2 * cur_len = 2^k
-    int cur_len = 1;
-    std::vector<int> sorted_by2(n, 0);
-    std::vector<int> new_classes(n, 0);
-    while (cur_len <= n){
+
+    for (int cur_len = 1; cur_len <= n; cur_len *= 2) {
         // сортируем по второй половине подстроки
         for (int i = 0; i < n; ++i){
             sorted_by2[i] = (suffs[i] + n - cur_len) % n;
         }
         // сортируем по первой половине
         // сортировка устойчивая, значит получим целиком отсортированные подстроки
-        for (int i = 0; i < cnt_len; ++i){ // обнуляем cnt
+        // обнуляем cnt
+        for (int i = 0; i < n; ++i){
             cnt[i] = 0;
         }
 
@@ -58,79 +77,87 @@ void sufix_array(std::string& s, int alphabet, std::vector<int>& suffs) {
             ++cnt[classes[sorted_by2[i]]];
         }
 
-        for (int i = 1; i < cnt_len; ++i) // считаем границы
-            cnt[i] += cnt[i-1];
-
+        for (int i = 1; i < n; ++i) {// считаем границы
+            cnt[i] += cnt[i - 1];
+        }
 
         for (int i = n - 1; i >= 0; --i){
             suffs[--cnt[classes[sorted_by2[i]]]] = sorted_by2[i];
         }
 
-        class_numb = 0;
+        //подсчтываем классы заново
+        int class_numb = 0;
         for (int i = 1; i < n; ++i){
             int mid1 = (suffs[i] + cur_len) % n;
             int mid2 = (suffs[i - 1] + cur_len) % n;
-            if (classes[suffs[i]] != classes[suffs[i - 1]] or classes[mid1] != classes[mid2])
+            if (classes[suffs[i]] != classes[suffs[i - 1]] || classes[mid1] != classes[mid2]) {
                 ++class_numb;
+            }
             new_classes[suffs[i]] = class_numb;
         }
-        for (int i = 0; i < n; ++i){
-            classes[i] = new_classes[i];
-        }
-        cur_len *= 2;
+
+        classes = new_classes;
     }
 }
 
-void lcp_array(std::string& s, std::vector<int>& suffs, std::vector<int>& lcp){
-    int n = s.length();
+
+std::vector<int> make_suff_array (const std::string& s, const int alphabet) {
+    std::vector<int> suffs = zero_iteration(s, alphabet);
+    std::vector<int> classes = make_classes(s, suffs);
+    main_iterations(suffs, classes);
+    return suffs;
+}
+
+
+std::vector<int> make_lcp_array(const std::string& s, const std::vector<int>& suffs) {
+    const int n = s.length();
+    std::vector<int> lcp (n, 0);
     int k = 0;
     std::vector<int> pos(n);
-    for(int i = 0; i < n; ++i) pos[suffs[i]] = i; // pos = suffs^-1
+    for(int i = 0; i < n; ++i) {
+        pos[suffs[i]] = i; // pos = suffs^-1
+    }
 
     for(int i = 0; i < n; ++i){
-        if (k > 0) --k;
-        if (pos[i] == n - 1){
+        if (k > 0) {
+            --k;
+        }
+        if (pos[i] == n - 1) {
             k = 0;
             lcp[n - 1] = -1;
-        }else{
+        } else {
             int j = suffs[pos[i] + 1];
-            while ((i + k > j + k ? i + k : j + k) < n and s[i + k] == s[j + k])
+            while ((i + k > j + k ? i + k : j + k) < n and s[i + k] == s[j + k]) {
                 ++k;
+            }
             lcp[pos[i]] = k;
         }
     }
 
-    for(int i = n - 1; i > 0; --i){
-        lcp[i] = lcp[i - 1];
-    }
-    lcp[0] = -1;
+    return lcp;
 }
 
-bool is_changed(std::vector<int>& p, int i, int pivot){
+
+bool is_changed (const std::vector<int>& p, const int i, const int pivot) {
     return (p[i] < pivot && p[i - 1] > pivot) ||
            (p[i] > pivot && p[i - 1] < pivot);
 }
 
 
-std::string find_k_substring(std::string& s, std::string& l, int64_t k){
-    int old_s_size = s.size();
+std::string find_k_substring(std::string& s, const std::string& l, int64_t k){
+    const int old_s_size = s.size();
     s = s + "#" + l + "$";
 
-    std::vector<int> p(s.size(), 0);
-    std::vector<int> lcp(s.size(), 0);
-    sufix_array(s, 256, p);
-    lcp_array(s, p, lcp);
-
-    for (int i = 0; i < s.size(); ++i) {
-        //std::cout << p[i] << "|" << lcp[i] << " \n";
-    }
+    std::vector<int> p;
+    std::vector<int> lcp;
+    p = make_suff_array(s, 256);
+    lcp = make_lcp_array(s, p);
 
     int lcp_old = 0;
     std::string ans;
 
     for (int i = 1; i < s.size(); ++i){
-        if (is_changed(p, i, old_s_size)){
-            //std::cout << p[i - 1] << " comp with " << p[i] << " " << "(" << lcp[i] << ") " << k << "\n";
+        if (is_changed(p, i, old_s_size)) {
             if (lcp_old + k > lcp[i]) {
                 if (lcp[i] >= lcp_old) {
                     k -= lcp[i] - lcp_old;
@@ -146,13 +173,14 @@ std::string find_k_substring(std::string& s, std::string& l, int64_t k){
                 break;
             }
         }
-        if (lcp[i] < lcp_old){
+        if (lcp[i] < lcp_old) {
             lcp_old = lcp[i];
         }
     }
 
-    if (k > 0)
+    if (k > 0) {
         ans = "-1";
+    }
 
     return ans;
 }
@@ -164,7 +192,7 @@ int main() {
     std::cin >> l;
     std::cin >> k;
 
-
     std::cout << find_k_substring(s, l, k);
+
     return 0;
 }
