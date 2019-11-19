7d0726b8de32b88fe5e0501bc2a69596b8c1606c
SweetWeakness <shirshin.dg@phystech.edu>
2019-11-19 19:30:14 +0300

    b1 remake

--- mod1_b1.cpp
+++ mod1_b1.cpp
@@ -1,26 +1,35 @@
+/*
+ * Найти лексикографически-минимальную строку, построенную по префикс-функции, в алфавите a-z.
+ */
+
+
 #include <iostream>
 #include <vector>
 
-const int alphabet = 26;
 
-char new_character(std::string& s, std::vector<int>& pi, int index) {
+const int alphabet = 26;
+const char zero_chr = 'a';
+
+
+char new_character(std::string& s, const std::vector<int>& pi, int index) {
     std::vector<bool> available_symbols(alphabet, true);
 
     while (index > 0) {
         index = pi[index - 1];
-        available_symbols[s[index] - 'a'] = false;
+        available_symbols[s[index] - zero_chr] = false;
     }
 
     for (int i = 0; i < alphabet; ++i) {
         if (available_symbols[i]) {
-            return 'a' + i;
+            return zero_chr + i;
         }
     }
 
 }
 
-std::string buildFromPrefix(std::vector<int>& pi) {
-    std::string s = "";
+std::string buildFromPrefix(const std::vector<int>& pi) {
+    std::string s;
+
     for(int i = 0; i<pi.size(); i++){
         if (pi[i] == 0){
             s += new_character(s, pi, i);
@@ -28,6 +37,7 @@ std::string buildFromPrefix(std::vector<int>& pi) {
             s += s[pi[i] - 1];
         }
     }
+
     return s;
 }
 
